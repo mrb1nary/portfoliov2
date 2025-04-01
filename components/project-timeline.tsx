c9e272b072a49052bf1e534ce8cbe61a7343efb5
mrb1nary <anubhab.techie@gmail.com>
2025-04-01 13:06:10 +0530

    Compressed the videos to webm

--- components/project-timeline.tsx
+++ components/project-timeline.tsx
@@ -82,7 +82,7 @@ export function ProjectTimeline() {
               preload="auto"
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             >
-              <source src="/assets/soltradingbotvideo.mp4" type="video/mp4" />
+              <source src="/assets/soltradingbotvideo.webm" type="video/webm" />
               Cant load video
             </video>
             <Image
@@ -117,7 +117,7 @@ export function ProjectTimeline() {
               preload="auto"
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             >
-              <source src="/assets/rustbc.mp4" type="video/mp4" />
+              <source src="/assets/rustbc.webm" type="video/webm" />
               Cant load video
             </video>
             <Image
